ca15d906de4833fc44a4a3e9ffc26a7bdd9a423b
MEDISETTI BALA CHELLARAO <2300031585@kluniversity.in>
2025-11-12 02:07:13 +0530

    Restrict CORS to specific origin for ResultsController

--- src/main/java/com/example/demo/controller/ResultsController.java
+++ src/main/java/com/example/demo/controller/ResultsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/quizzes")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:30082")
 public class ResultsController {
 
     @Autowired
